ae06ca33377621e53366dbfba76e280a70aa7df0
nature <nature_lei@outlook.com>
2024-05-24 11:31:48 +0800

    feat: 文本解析处理器+一些格式上的更改

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import time
 
 from Dispatcher.dispatcher import Dispatcher
-from FileProcessors.Generators.all_to_one_excel_generator import AllToOneExcelGenerator
 from FileProcessors.Generators.one_to_one_text_generator import OneToOneTextGenerator
 from FileTraverse.TraverseStrategies.traverse_strategy_action_model_enum import TraverseStrategyActionModelEnum
 
 if __name__ == '__main__':
-    # root_directory = r"your\file\path"  # 根目录
-    # generate_path = r"file\generation\path"
-
-    root_directory = r"C:\Users\yuese\Desktop\新建文件夹 (2)\11223论语.t23xt"  # 根目录
-    generate_path = r"C:\Users\yuese\Desktop\新建文件夹"
+    root_directory = r"your\file\path"  # 根目录
+    generate_path = r"file\generation\path"
 
     specify_processors = ["textParseProcessor"]
     generators = list()
